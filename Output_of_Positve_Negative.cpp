22bb4c256ea55c5af6a5274d2f9295e37eef0f42
Mansoor Ali <76454824+Mansoor-Ali-0701@users.noreply.github.com>
2021-07-06 15:19:36 -0700

    Update Output_of_Positve_Negative.cpp

--- Output_of_Positve_Negative.cpp
+++ Output_of_Positve_Negative.cpp
@@ -1,3 +1,6 @@
+/*
+Write a program that prompts the user to input a number. The program should then output the number and a message saying whether the number is positive, negative, or zero.
+*/
 #include <iostream>
 
 using namespace std;
